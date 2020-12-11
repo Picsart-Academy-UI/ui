a7f94811b0afdb15a9b45203b4b1bc543f7d40a7
varszakaryan <varserzakaryan@gmail.com>
2020-12-11 18:51:31 +0400

    file structure guide

---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+// WHAT TO IMPORT AND WHERE
+// 1. import the external packages, tech tools here
+// 2. import local files, component
+// 3. import styles
+
+// FUNCTIONAL COMPONENT DETAILS
+
+// EXPORT AT THE END OF THE FILE
+
