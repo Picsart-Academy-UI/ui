de6df0811c7ff58436f31b3a809593b3d0012896
Arshak Grigoryan <52595017+arshak-grigoryan@users.noreply.github.com>
2020-12-14 16:17:12 +0400

    Code styling (#5)
    * install styling packages
    
    * add import/order
    
    * lint fix
    
    * change lintstaged file format

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: [
+    "react-app",
+    "airbnb/base",
+    "prettier",
+  ],
+  "plugins": [
+    "prettier",
+  ],
+  rules: {
+    "quotes": ["warn", "single"],
+    "no-param-reassign": "off",
+    "import/order": ["error", {
+      "groups": ["builtin", "external", "parent", "sibling", "index"],
+      "newlines-between": "always-and-inside-groups",
+    }]
+  },
+  overrides: [
+    {
+      "files": [
+        "**/*.ts?(x)"
+      ],
+      "rules": {
+        "additional-typescript-only-rule": "warn"
+      }
+    }
+  ],
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js",".jsx",".ts",".tsx",".json",]
+      }
+    }
+  }
+}
--- src/testStore/index.js
+++ src/testStore/index.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import testReducer from './slices/TestSlice';
 
 export default configureStore({
