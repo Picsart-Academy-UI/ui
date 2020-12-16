b833296c7a1c3e48fbd152f127e5e084f3864d6d
varszakaryan <varserzakaryan@gmail.com>
2020-12-16 19:48:31 +0400

    added routing setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import Router from './routes';
+
 import Homepage from './pages/Login/components/Homepage';
 
 function App() {
-  return <Homepage />;
+  return (
+    <BrowserRouter>
+      <Router>
+        <Homepage />;
+      </Router>
+    </BrowserRouter>
+  );
 }
 
 export default App;
