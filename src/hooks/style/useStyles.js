78311dd9f2d157e486f3122c5562a92466a24220
Roman Balayan <romanpicsart@Romans-MacBook-Pro.local>
2020-12-17 15:03:59 +0400

    homepage redisign

--- src/hooks/style/useStyles.js
+++ src/hooks/style/useStyles.js
@@ -11,38 +11,36 @@ const useStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
   },
-  MuiContainerRoot: {
-    float: 'left',
-    marginLeft: theme.spacing(5),
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    marginRight: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
   header: {
+    display: 'flex',
     fontSize: '1.2rem',
     '@media (min-width:600px)': {
-      fontSize: '4.5rem',
+      fontSize: '3.8rem',
     },
     fontFamily: 'Alieron',
     letterSpacing: 7,
     marginLeft: theme.spacing(5),
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(3),
+    justifyContent: 'center',
   },
-  form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
+  signInContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing(6),
   },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-    backgroundColor: 'Transparent',
-    outline: 'none',
-    '&:hover': {
-      backgroundColor: 'Transparent',
+  glogo: {
+    width: '34px',
+    height: '34px',
+    marginRight: theme.spacing(1),
+  },
+  buttonPcsrt: {
+    background:
+      '-webkit-linear-gradient(100deg, rgba(219,68,218,1) 0%, rgba(89,192,246,1) 78%, rgba(58,231,255,1) 100%)',
+    fontSize: '0.1rem',
+    '@media (min-width:600px)': {
+      fontSize: '1.0rem',
     },
+    font: 'initial',
   },
 }));
 
--- src/pages/Login/components/Homepage.js
+++ src/pages/Login/components/Homepage.js
@@ -1,13 +1,8 @@
-import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import { Avatar, Button } from '@material-ui/core';
 
 import useStyles from '../../../hooks/style/useStyles';
 
@@ -16,61 +11,24 @@ function Homepage() {
 
   return (
     <div className={classes.paperContainer}>
-      <Typography component="h1" variant="h2" className={classes.header}>
-        Office Space Management
+      <Typography component="h1" variant="h4" className={classes.header}>
+        We Make Office Space Management
       </Typography>
-      <Container
-        component="main"
-        maxWidth="xs"
-        className={classes.MuiContainerRoot}
+      <Typography
+        component="h1"
+        variant="h4"
+        className={`${classes.header} ${classes.colored}`}
       >
+        AWESOME
+      </Typography>
+      <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <div className={classes.paper}>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-        <Box mt={8}></Box>
+        <Box className={classes.signInContainer}>
+          <Button className={classes.buttonPcsrt}>
+            <Avatar src="images/glogo.png" className={classes.glogo} />
+            Sign In With Google
+          </Button>
+        </Box>
       </Container>
     </div>
   );
