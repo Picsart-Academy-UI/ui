8239881ab742965ac6b58151d1c89db026db6edb
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-22 03:42:52 +0400

    delete redux-thunk package

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage/session';
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
-import thunk from 'redux-thunk';
 
 import testReducer from './slices/TestSlice';
 import signinReducer from './slices/signinSlice';
@@ -22,7 +21,6 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 const store = configureStore({
   reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production',
-  middleware: [thunk],
 });
 
 export default store;
