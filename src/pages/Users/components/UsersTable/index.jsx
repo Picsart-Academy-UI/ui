e6ef164ada6e92e929a746d3b733fe202b995508
jir999 <jir999@users.noreply.github.com>
2020-12-28 12:03:04 +0400

    Adding Pagination

--- src/pages/Users/components/UsersTable/index.jsx
+++ src/pages/Users/components/UsersTable/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -7,6 +8,7 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import UserRow from '../UserRow';
+import TablePagination from '../TablePagination';
 
 const createData = (name, surName, team, gmail, position, birthDay) => ({
   name,
@@ -60,30 +62,117 @@ const rows = [
     'Middle 1',
     '17.06.1997'
   ),
+  createData(
+    'User 6',
+    'Ananyan',
+    'Team 6',
+    'yu456.ananyan@gmail.com',
+    'Middle 3',
+    '17.06.1997'
+  ),
+  createData(
+    'User 7',
+    'Ananyan',
+    'Team 7',
+    'kf478.ananyan@gmail.com',
+    'Middle 1',
+    '17.06.1997'
+  ),
+  createData(
+    'User 8',
+    'Ananyan',
+    'Team 8',
+    'io478.ananyan@gmail.com',
+    'Middle 1',
+    '17.06.1997'
+  ),
+  createData(
+    'User 9',
+    'Ananyan',
+    'Team 9',
+    'po478.ananyan@gmail.com',
+    'Middle 1',
+    '17.06.1997'
+  ),
+  createData(
+    'User 10',
+    'Ananyan',
+    'Team 10',
+    'lm478.ananyan@gmail.com',
+    'Middle 1',
+    '17.06.1997'
+  ),
+  createData(
+    'User 11',
+    'Dalakyan',
+    'Team 11',
+    'us231.dalakyan@gmail.com',
+    'Junior 1',
+    '12.10.1999'
+  ),
 ];
 
-const UsersTable = () => (
-  <TableContainer component={Paper}>
-    <Table aria-label="collapsible table">
-      <TableHead>
-        <TableRow>
-          <TableCell />
-          <TableCell>Name</TableCell>
-          <TableCell align="center">SurName</TableCell>
-          <TableCell align="center">Team</TableCell>
-          <TableCell align="center">Gmail</TableCell>
-          <TableCell align="right">
-            <Box mr={5}>Actions</Box>
-          </TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <UserRow key={row.name} row={row} />
-        ))}
-      </TableBody>
-    </Table>
-  </TableContainer>
-);
+const UsersTable = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (value) => {
+    setRowsPerPage(value);
+    setPage(0);
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
+
+  return (
+    <Paper>
+      <TableContainer>
+        <Table aria-label="collapsible table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              <TableCell>Name</TableCell>
+              <TableCell align="center">SurName</TableCell>
+              <TableCell align="center">Team</TableCell>
+              <TableCell align="center">Gmail</TableCell>
+              <TableCell align="right">
+                <Box mr={5}>Actions</Box>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : rows
+            ).map((row) => (
+              <UserRow key={row.name} row={row} />
+            ))}
+
+            {emptyRows > 0 && (
+              <TableRow>
+                <TableCell />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rows={rows}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </TableContainer>
+    </Paper>
+  );
+};
 
 export default UsersTable;
+
+// <TableRow style={{ height: 53 * emptyRows }}>
+//                 <TableCell colSpan={6} />
+//               </TableRow>
