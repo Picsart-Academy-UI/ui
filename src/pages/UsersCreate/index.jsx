448e797e0435a6554719946e1fe2c5c806d74ac1
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-01 00:28:29 +0400

    change team field, add birthdate, phone number

--- src/pages/UsersCreate/index.jsx
+++ src/pages/UsersCreate/index.jsx
@@ -1,16 +1,32 @@
+import { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import useStylesLocal from './style';
 
 const UsersCreate = () => {
-  const styles = useStylesLocal();
+  const classesLocal = useStylesLocal();
+
+  const [dateType, setDateType] = useState('text');
+
+  const onDateTypeFocus = () => {
+    console.log(dateType);
+    setDateType('date');
+  };
+
+  const onDateTypeBlur = () => {
+    console.log(dateType);
+    setDateType('text');
+  };
+
+  const [checkedB, setCheckedB] = useState(true);
+
+  const handleChange = (event) => {
+    setCheckedB(event.target.checked);
+  };
 
   return (
     <div>
@@ -44,16 +60,26 @@ const UsersCreate = () => {
               id="surname"
               label="Surname"
             />
-            <FormControl variant="outlined" fullWidth>
-              <InputLabel htmlFor="outlined-team-native-simple">
-                Team
-              </InputLabel>
-              <Select native id="grouped-native-select" label="Team">
-                <option aria-label="None" value="" />
-                <option>Team 1</option>
-                <option>Team 2</option>
-              </Select>
-            </FormControl>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              id="date"
+              label="Birthday"
+              type={dateType}
+              onFocus={onDateTypeFocus}
+              onBlur={onDateTypeBlur}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="team"
+              label="Team"
+              defaultValue="5fe23d54a710eb52a9fe0835"
+              disabled
+            />
             <TextField
               variant="outlined"
               margin="normal"
@@ -62,17 +88,32 @@ const UsersCreate = () => {
               id="position"
               label="Position"
             />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="phoneNumber"
+              label="Phone Number"
+            />
             <FormControlLabel
-              control={<Checkbox name="checkedB" color="primary" />}
-              label="Is Admin"
+              control={
+                <Checkbox
+                  checked={checkedB}
+                  onChange={handleChange}
+                  name="admin"
+                  color="primary"
+                />
+              }
+              label="Admin"
             />
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              className={styles.sbmtButton}
+              className={classesLocal.sbmtButton}
             >
-              Save and Send Invitation
+              Send Invitation
             </Button>
           </form>
         </div>
