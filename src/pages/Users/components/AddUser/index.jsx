cc7afd88d97bc5c8ac15d5b318fb827e266eb432
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2020-12-31 14:01:16 +0400

    handle addUser click

--- src/pages/Users/components/AddUser/index.jsx
+++ src/pages/Users/components/AddUser/index.jsx
@@ -1,13 +1,15 @@
+import { useHistory } from 'react-router-dom';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
 const AddUser = () => {
-  const handleAddClick = () => {};
+  const history = useHistory();
+  const onAddUserClick = () => history.push('/users/create');
 
   return (
     <Box display="flex" justifyContent="flex-end">
       <Box mr={3} m={1}>
-        <Button onClick={handleAddClick} color="primary" variant="contained">
+        <Button onClick={onAddUserClick} color="primary" variant="contained">
           Add User
         </Button>
       </Box>
