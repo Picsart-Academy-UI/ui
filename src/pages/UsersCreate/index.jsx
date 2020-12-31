b1ccc964d6a9756acafd7c0276ff59293a7b02c0
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-01 03:38:08 +0400

    add user invite handler

--- src/pages/UsersCreate/index.jsx
+++ src/pages/UsersCreate/index.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import useFetch from '../../hooks/useFetch';
+import getUserInvitationRequestData from './service/getUserInvitationRequestData';
 import useStylesLocal from './style';
 
 const UsersCreate = () => {
+  const token = useSelector((state) => state.signin.token);
+  const makeRequest = useFetch();
   const classesLocal = useStylesLocal();
 
   const [dateType, setDateType] = useState('text');
@@ -28,12 +33,61 @@ const UsersCreate = () => {
     setCheckedB(event.target.checked);
   };
 
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const surnameRef = useRef();
+  const birthDayRef = useRef();
+  const teamRef = useRef();
+  const positionRef = useRef();
+  const phoneNumberRef = useRef();
+  const adminRef = useRef();
+
+  const onSendInvitationSubmit = async (e) => {
+    e.preventDefault();
+
+    const body = {
+      is_admin: adminRef.current.checked,
+      email: emailRef.current.value,
+      team_id: teamRef.current.value,
+      position: positionRef.current.value,
+      first_name: nameRef.current.value,
+      last_name: surnameRef.current.value,
+      birthdate: birthDayRef.current.value,
+      phonenumber: phoneNumberRef.current.value,
+    };
+
+    const { url, options } = getUserInvitationRequestData({ token, body });
+
+    try {
+      const res = await makeRequest(url, options);
+
+      console.log(res);
+      if (res.success) {
+        console.log(res.msg);
+
+        adminRef.current.checked = false;
+        emailRef.current.value = '';
+        teamRef.current.value = '';
+        positionRef.current.value = '';
+        nameRef.current.value = '';
+        surnameRef.current.value = '';
+        birthDayRef.current.value = '';
+        setDateType('text');
+        phoneNumberRef.current.value = '';
+        return true;
+      }
+      return false;
+    } catch (err) {
+      return new Error(err.message);
+    }
+  };
+
   return (
     <div>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <div>
-          <form noValidate>
+          <form noValidate={false} onSubmit={onSendInvitationSubmit}>
             <TextField
               variant="outlined"
               margin="normal"
@@ -43,6 +97,7 @@ const UsersCreate = () => {
               label="Email Address"
               name="email"
               autoFocus
+              inputRef={emailRef}
             />
             <TextField
               variant="outlined"
@@ -51,6 +106,7 @@ const UsersCreate = () => {
               fullWidth
               id="name"
               label="Name"
+              inputRef={nameRef}
             />
             <TextField
               variant="outlined"
@@ -59,6 +115,7 @@ const UsersCreate = () => {
               fullWidth
               id="surname"
               label="Surname"
+              inputRef={surnameRef}
             />
             <TextField
               variant="outlined"
@@ -69,6 +126,7 @@ const UsersCreate = () => {
               type={dateType}
               onFocus={onDateTypeFocus}
               onBlur={onDateTypeBlur}
+              inputRef={birthDayRef}
             />
             <TextField
               variant="outlined"
@@ -79,6 +137,7 @@ const UsersCreate = () => {
               label="Team"
               defaultValue="5fe23d54a710eb52a9fe0835"
               disabled
+              inputRef={teamRef}
             />
             <TextField
               variant="outlined"
@@ -87,14 +146,15 @@ const UsersCreate = () => {
               fullWidth
               id="position"
               label="Position"
+              inputRef={positionRef}
             />
             <TextField
               variant="outlined"
               margin="normal"
-              required
               fullWidth
               id="phoneNumber"
               label="Phone Number"
+              inputRef={phoneNumberRef}
             />
             <FormControlLabel
               control={
@@ -103,6 +163,7 @@ const UsersCreate = () => {
                   onChange={handleChange}
                   name="admin"
                   color="primary"
+                  inputRef={adminRef}
                 />
               }
               label="Admin"
