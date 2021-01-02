5988728cb9854a7ea45beb3afe913e9e9e4e2792
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-03 03:51:44 +0400

    delete unnecessary code, add reusable Input

--- src/pages/UsersInvite/index.jsx
+++ src/pages/UsersInvite/index.jsx
@@ -1,13 +1,13 @@
 import { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import useFetch from '../../hooks/useFetch';
 import getUserInvitationRequestData from '../../services/users/getUserInvitationRequestData';
+import Input from './components/Input';
 import useStylesLocal from './style';
 
 const UsersInvite = () => {
@@ -83,103 +83,58 @@ const UsersInvite = () => {
   };
 
   return (
-    <div>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div>
-          <form noValidate={false} onSubmit={onSendInvitationSubmit}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoFocus
-              inputRef={emailRef}
+    <Container component="main" maxWidth="xs">
+      <form noValidate={false} onSubmit={onSendInvitationSubmit}>
+        <Input id="email" label="Email Address" inputRef={emailRef} autoFocus />
+        <Input id="name" label="Name" inputRef={nameRef} />
+        <Input id="surname" label="Surname" inputRef={surnameRef} />
+        <TextField
+          required={false}
+          variant="outlined"
+          margin="normal"
+          fullWidth
+          id="date"
+          label="Birthday"
+          type={dateType}
+          onFocus={onDateTypeFocus}
+          onBlur={onDateTypeBlur}
+          inputRef={birthDayRef}
+        />
+        <Input
+          id="team"
+          label="Team"
+          defaultValue="5fe23d54a710eb52a9fe0835"
+          disabled
+          inputRef={teamRef}
+        />
+        <Input id="position" label="Position" inputRef={positionRef} />
+        <Input
+          required={false}
+          id="phoneNumber"
+          label="Phone Number"
+          inputRef={phoneNumberRef}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={checkedB}
+              onChange={handleChange}
+              color="primary"
+              inputRef={adminRef}
             />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Name"
-              inputRef={nameRef}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="surname"
-              label="Surname"
-              inputRef={surnameRef}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="date"
-              label="Birthday"
-              type={dateType}
-              onFocus={onDateTypeFocus}
-              onBlur={onDateTypeBlur}
-              inputRef={birthDayRef}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="team"
-              label="Team"
-              defaultValue="5fe23d54a710eb52a9fe0835"
-              disabled
-              inputRef={teamRef}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="position"
-              label="Position"
-              inputRef={positionRef}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              id="phoneNumber"
-              label="Phone Number"
-              inputRef={phoneNumberRef}
-            />
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={checkedB}
-                  onChange={handleChange}
-                  name="admin"
-                  color="primary"
-                  inputRef={adminRef}
-                />
-              }
-              label="Admin"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={classesLocal.sbmtButton}
-            >
-              Send Invitation
-            </Button>
-          </form>
-        </div>
-      </Container>
-    </div>
+          }
+          label="Admin"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          className={classesLocal.sbmtButton}
+        >
+          Send Invitation
+        </Button>
+      </form>
+    </Container>
   );
 };
 
