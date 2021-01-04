c1c92091e1c03f3696cab06d3288f9e34624f953
Roman Balayan <romanpicsart@Romans-MacBook-Pro.local>
2021-01-05 00:36:37 +0400

    removed hardcoded token

--- src/pages/Profile/components/TeamList.jsx
+++ src/pages/Profile/components/TeamList.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import useFetch from '../../../hooks/useFetch';
 import getAllTeams from '../../../services/profile/getAllTeams';
 
@@ -6,12 +7,13 @@ function TeamList(props) {
   const makeRequest = useFetch();
 
   const { curUserTeam } = props;
+  const token = useSelector((state) => state.signin.token);
 
   const [teams, setTeams] = useState(null);
 
   useEffect(() => {
     const fetchTeams = async () => {
-      const { url, options } = getAllTeams();
+      const { url, options } = getAllTeams(token);
       const data = await makeRequest(url, options);
       console.log(data.teams);
       setTeams(data.teams);
--- src/services/profile/getAllTeams.js
+++ src/services/profile/getAllTeams.js
@@ -1,9 +1,6 @@
 import { MAIN_ROUTE } from '../../constants';
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmViNzQzZWFjNWI2NzM0N2M1MTY1MjUiLCJlbWFpbCI6InJvbWFuLmJhbGF5YW5AcGljc2FydC5jb20iLCJ0ZWFtX2lkIjoiNWZlYjczMTAzMzliMGQzM2QxOGI3Mjg1IiwiaXNfYWRtaW4iOnRydWUsImlhdCI6MTYwOTI2NjM3Mn0.Om3K-0j4OhlF3wZD7AJFEqVONQ35LW6BhotFNZPtImQ';
-
-const getAllTeams = () => ({
+const getAllTeams = (token) => ({
   url: `${MAIN_ROUTE}/teams`,
   options: {
     method: 'GET',
