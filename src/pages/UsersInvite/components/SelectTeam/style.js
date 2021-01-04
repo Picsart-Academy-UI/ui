249c10af7c11fe6681b4f8a6700bd489a472209a
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-04 15:19:44 +0400

    add SelectTeam style

---
+++ src/pages/UsersInvite/components/SelectTeam/style.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStylesLocal = makeStyles(() => ({
+  formControl: {
+    minWidth: 120,
+  },
+}));
+
+const getStyleMenuItem = (name, personName, theme) => ({
+  fontWeight:
+    personName.indexOf(name) === -1
+      ? theme.typography.fontWeightLight
+      : theme.typography.fontWeightBold,
+});
+
+export { getStyleMenuItem };
+export default useStylesLocal;
