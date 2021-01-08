656de998ca1237861098a1b7e8a81f770c6de036
Hamlet <67255886+HamletGal@users.noreply.github.com>
2021-01-08 17:04:52 +0400

    bugfix

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Grid from '@material-ui/core/Grid';
+import { Grid } from '@material-ui/core';
 import { useDispatch, useSelector } from 'react-redux';
 import useFetch from '../../hooks/useFetch';
 import { getLimitedUsersData } from '../../services/users';
