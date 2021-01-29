b0519417fcba10d4ea410a3480148374810dedbb
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-30 03:43:37 +0400

    delete notification icon, unused styles, refactor styles

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStylesLocal = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
-  },
   header: {
     // backgroundColor: '#3a76e8',
     backgroundColor: 'rgb(35, 97, 211)',
@@ -15,39 +11,24 @@ const useStylesLocal = makeStyles((theme) => ({
     justifyContent: 'space-between',
     paddingLeft: theme.spacing(4.5),
   },
-  grow: {
-    flexGrow: 1,
+  linkTab: {
+    paddingTop: '18px',
+    paddingBottom: '18px',
   },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  desktopMenuRight: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  mobileMenuRightIcon: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  mobileMenuLeftIcon: {
+  mobileMenuIcon: {
     marginLeft: 0,
     display: 'flex',
     [theme.breakpoints.up('md')]: {
       display: 'none',
     },
   },
-  linkTab: {
-    paddingTop: '18px',
-    paddingBottom: '18px',
-  },
   desktopMenuLeft: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  desktopMenuRight: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
       display: 'flex',
