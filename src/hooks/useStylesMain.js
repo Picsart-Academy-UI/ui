8fd9299269a15f09f30f293317870189a0b60fac
arshak-grigoryan <arshakgrigoryan1796@gmail.com>
2021-01-30 21:42:49 +0400

    override some mui styles in userinvite

--- src/hooks/useStylesMain.js
+++ src/hooks/useStylesMain.js
@@ -32,7 +32,7 @@ const useStylesMain = makeStyles((theme) => ({
     padding: theme.spacing(1, 2),
     background:
       '-webkit-linear-gradient(120deg, rgba(219,68,218,1) 0%, rgba(89,192,246,1) 78%, rgba(58,231,255,1) 100%)',
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(3),
     fontWeight: 500,
     color: 'white',
   },
--- src/pages/UsersInvite/components/Form/index.jsx
+++ src/pages/UsersInvite/components/Form/index.jsx
@@ -5,13 +5,14 @@ import {
   Checkbox,
   CircularProgress,
 } from '@material-ui/core';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import useStylesMain from '../../../../hooks/useStylesMain';
 import Input from '../Input';
 import InputDate from '../InputDate';
 import SelectTeam from '../SelectTeam';
 import validate from './helpers/validateInfo';
 import useForm from './helpers/useForm';
-import useStylesLocal from './style';
+import useStylesLocal, { theme } from './style';
 
 const Form = ({ submitForm, isRequestNow }) => {
   const classesMain = useStylesMain();
@@ -23,87 +24,94 @@ const Form = ({ submitForm, isRequestNow }) => {
     submitForm,
     validate
   );
+
   return (
-    <div className={classesLocal.formWrapper}>
+    <MuiThemeProvider theme={theme}>
       <form onSubmit={handleSubmit}>
         <div className={classesLocal.inputsWrapper}>
-          <Input
-            id="email"
-            label="Email Address"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            error={Boolean(errors.email)}
-            helperText={errors.email}
-            autoFocus
-          />
-          <Input
-            id="first_name"
-            label="Name"
-            name="first_name"
-            value={values.first_name}
-            onChange={handleChange}
-            error={Boolean(errors.first_name)}
-            helperText={errors.first_name}
-          />
-          <Input
-            id="last_name"
-            label="Surname"
-            name="last_name"
-            value={values.last_name}
-            onChange={handleChange}
-            error={Boolean(errors.last_name)}
-            helperText={errors.last_name}
-          />
-          <InputDate
-            type={dateType}
-            name="birthday"
-            value={values.birthday}
-            onChange={handleChange}
-            error={Boolean(errors.birthday)}
-            helperText={errors.birthday}
-            setDateType={setDateType}
-          />
-          <Input
-            id="phone"
-            label="Phone"
-            name="phone"
-            value={values.phone}
-            onChange={handleChange}
-            error={Boolean(errors.phone)}
-            helperText={errors.phone}
-            type="number"
-          />
-          <SelectTeam
-            id="team_id"
-            team_id="team_id"
-            value={values.team_id}
-            onChange={handleChange}
-            error={Boolean(errors.team_id)}
-            helperText={errors.team_id}
-          />
-          <Input
-            id="position"
-            label="Position"
-            name="position"
-            value={values.position}
-            onChange={handleChange}
-            error={Boolean(errors.position)}
-            helperText={errors.position}
-          />
-          <FormControlLabel
-            className={classesLocal.checkbox}
-            control={
-              <Checkbox
-                id="is_admin"
-                name="is_admin"
-                value={values.is_admin}
+          <div className={classesLocal.inputsWrapper2}>
+            <div className={classesLocal.columnWrapper}>
+              <Input
+                id="email"
+                label="Email Address"
+                name="email"
+                value={values.email}
                 onChange={handleChange}
-                color="primary"
+                error={Boolean(errors.email)}
+                helperText={errors.email}
+                autoFocus
               />
-            }
-            label="Admin"
-          />
+              <Input
+                id="first_name"
+                label="Name"
+                name="first_name"
+                value={values.first_name}
+                onChange={handleChange}
+                error={Boolean(errors.first_name)}
+                helperText={errors.first_name}
+              />
+              <Input
+                id="last_name"
+                label="Surname"
+                name="last_name"
+                value={values.last_name}
+                onChange={handleChange}
+                error={Boolean(errors.last_name)}
+                helperText={errors.last_name}
+              />
+              <InputDate
+                type={dateType}
+                name="birthday"
+                value={values.birthday}
+                onChange={handleChange}
+                error={Boolean(errors.birthday)}
+                helperText={errors.birthday}
+                setDateType={setDateType}
+              />
+            </div>
+            <div className={classesLocal.columnWrapper}>
+              <Input
+                id="phone"
+                label="Phone"
+                name="phone"
+                value={values.phone}
+                onChange={handleChange}
+                error={Boolean(errors.phone)}
+                helperText={errors.phone}
+                type="number"
+              />
+              <SelectTeam
+                id="team_id"
+                team_id="team_id"
+                value={values.team_id}
+                onChange={handleChange}
+                error={Boolean(errors.team_id)}
+                helperText={errors.team_id}
+              />
+              <Input
+                id="position"
+                label="Position"
+                name="position"
+                value={values.position}
+                onChange={handleChange}
+                error={Boolean(errors.position)}
+                helperText={errors.position}
+              />
+              <FormControlLabel
+                className={classesLocal.checkbox}
+                control={
+                  <Checkbox
+                    id="is_admin"
+                    name="is_admin"
+                    value={values.is_admin}
+                    onChange={handleChange}
+                    color="primary"
+                  />
+                }
+                label="Admin"
+              />
+            </div>
+          </div>
           <Button
             type="submit"
             variant="contained"
@@ -111,14 +119,14 @@ const Form = ({ submitForm, isRequestNow }) => {
             disabled={isRequestNow}
           >
             {isRequestNow ? (
-              <CircularProgress size={24} style={{ color: 'white' }} />
+              <CircularProgress size={24} className={classesLocal.loader} />
             ) : (
               'Invite'
             )}
           </Button>
         </div>
       </form>
-    </div>
+    </MuiThemeProvider>
   );
 };
 
