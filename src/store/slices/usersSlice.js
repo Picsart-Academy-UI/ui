9b97de0d36eec8871a2dcddcc287a83888f80e77
jir999 <jir999@users.noreply.github.com>
2021-02-05 23:07:09 +0400

    moving Loading state into Redux

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -4,21 +4,31 @@ export const usersSlice = createSlice({
   name: 'usersData',
   initialState: {
     usersList: {},
+    isLoading: false,
   },
   reducers: {
     fetchedUsersList: (state, action) => {
       state.usersList = action.payload;
+      state.isLoading = false;
+    },
+    handleIsLoadingChange: (state) => {
+      state.isLoading = true;
     },
     deleteUser: (state, action) => {
       const userId = action.payload;
       const index = state.usersList.data.findIndex(({ _id }) => userId === _id);
       state.usersList.data.splice(index, 1);
+      state.isLoading = false;
       state.usersList.count--; // eslint-disable-line
     },
   },
 });
 
-export const { fetchedUsersList, deleteUser, searchUser } = usersSlice.actions;
+export const {
+  fetchedUsersList,
+  deleteUser,
+  handleIsLoadingChange,
+} = usersSlice.actions;
 
 export const getUsers = ({ users }) => users;
 
